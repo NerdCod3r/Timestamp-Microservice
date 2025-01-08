77a7c86e21ce9ba29d2c80d739637d863a4023ab
Apinda Tekula <nerdcod3rx@gmail.com>
2025-01-08 00:32:17 +0000

    I have completed the Timestamp microservice

--- index.js
+++ index.js
@@ -24,6 +24,60 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// The API endpoint for default usage
+app.get("/api/", function(req, res) {
+  res.json({"unix": parseInt(new Date().getTime()), "utc": new Date().toUTCString()});
+});
+
+// The API endpoint proper usage
+app.get("/api/:date", function(request, response){
+  var date_string = request.params.date;
+
+  // Milliseconds or a word(invalid)
+  if (date_string.split('-').length === 1 )
+  { 
+    // Validate the date(s)
+    const reg = /\s/;
+    var found = reg.test(date_string);
+    // console.log(found);
+    if ( found ) {
+      // Handle the date in Long format
+      if ( new Date(date_string) == 'Invalid Date' )
+      {
+         response.json({"error": "Invalid Date"});
+      } else {
+        response.json({"unix": parseInt(new Date(date_string).getTime()), "utc": new Date(date_string).toUTCString()});
+      }
+    } else if (!found) {
+      if (/[A-Za-z]/.test(date_string)) {
+        response.json({"error": "Invalid Date"});
+      }
+      else {
+        // Valid milliseconds date
+        var newdate = new Date(parseInt(date_string)).toUTCString();
+        response.json({"unix": parseInt(date_string), "utc": newdate});
+      }
+    }
+  }
+  // Valid format of the date YYYY-MM-DD  
+  else if ( date_string.split('-').length == 3 )
+    {
+      var new_date = new Date(date_string).toUTCString(); // valid for utc
+      // console.log(new_date === "Invalid Date");
+      // console.log(new Date(date_string).getTime());
+      if ( new_date === "Invalid Date" ) {
+        response.json({"error": "Invalid Date"});
+      } else{
+
+        var new_date_milliseconds = new Date(date_string).getTime();     // valid for unix
+        // console.log(new_date_milliseconds);
+        response.json({"unix": parseInt(new_date_milliseconds), "utc": new_date});
+      }
+    } else if ( date_string.split("-").length !=  2 )
+      {
+        response.json({"error": "Invalid Date"});
+    }
+});
 
 
 // Listen on port set in environment variable or default to 3000
